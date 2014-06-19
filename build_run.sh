b55d92ecb18ad39650a1f0facf89aa9c6aaa63a3
Tobias Gustafsson <tobias.l.gustafsson@gmail.com>
2014-06-19 22:46:57 +0200

    Small shell script to build and run tests

---
+++ build_run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Build C module and run tests if compilation succeeds.
+# Can be used together with nosier to create a decent code-build-test cycle
+# nosier "./build_run.sh"
+python setup.py build --debug && python setup.py install && py.test tests/ "$@"
