3605557988342260bf0b202b98ffcbbc73db24fd
Tobias Gustafsson <tobias.l.gustafsson@gmail.com>
2017-01-06 19:07:05 +0100

    Fix get_in for Python 3

--- pyrsistent/_toolz.py
+++ pyrsistent/_toolz.py
@@ -39,6 +39,7 @@ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 DAMAGE.
 """
 import operator
+from six.moves import reduce
 
 
 def get_in(keys, coll, default=None, no_default=False):
