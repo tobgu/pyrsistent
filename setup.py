1c6d7935cd1894d99379a1bcff6a9cd85f449a03
Tobias Gustafsson <tobias.l.gustafsson@gmail.com>
2017-10-08 17:33:59 +0200

    Drop official support for python 2.6 and try to get PyPy going on Travis again

--- setup.py
+++ setup.py
@@ -71,7 +71,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
