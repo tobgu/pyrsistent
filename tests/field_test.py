e6e96e51f59347fe32977377fdbabbcd13b977d1
Neil Vyas <neil.vyas@affirm.com>
2018-02-19 15:58:14 -0800

    Introducing some coverage for Enum types in seq_fields

--- tests/field_test.py
+++ tests/field_test.py
@@ -1,18 +1,23 @@
-from pyrsistent import field
+from pyrsistent._compat import Enum
+
+from pyrsistent import field, pvector_field
+
+
+class TestEnum(Enum):
+    x = 1
+    y = 2
 
 
 def test_enum():
-    try:
-        from enum import Enum
-    except ImportError:
-        # skip enum test if Enums are not available
-        return
-
-    class TestEnum(Enum):
-        x = 1
-        y = 2
-
     f = field(type=TestEnum)
 
     assert TestEnum in f.type
     assert len(f.type) == 1
+
+
+# This is meant to exercise `_seq_field`.
+def test_pvector_field_enum_type():
+    f = pvector_field(TestEnum)
+
+    assert len(f.type) == 1
+    assert TestEnum is list(f.type)[0].__type__
