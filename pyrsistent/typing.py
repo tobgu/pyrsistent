13e91baf4c84f77fe4689daa6a8968e0bdaee077
nattofriends <nattofriends@gmail.com>
2018-10-24 16:57:07 -0700

    Wrap everything in try-except because of doctest27

--- pyrsistent/typing.py
+++ pyrsistent/typing.py
@@ -12,53 +12,46 @@ For example,
 
 """
 
-from typing import Container
-from typing import Hashable
-from typing import Generic
-from typing import Iterable
-from typing import Mapping
-from typing import Sequence
-from typing import Sized
-from typing import TypeVar
+try:
+    from typing import Container
+    from typing import Hashable
+    from typing import Generic
+    from typing import Iterable
+    from typing import Mapping
+    from typing import Sequence
+    from typing import Sized
+    from typing import TypeVar
 
+    __all__ = ['CheckedPSet', 'CheckedPVector', 'PBag', 'PDeque', 'PList', 'PMap', 'PSet', 'PVector']
 
-__all__ = ['CheckedPSet', 'CheckedPVector', 'PBag', 'PDeque', 'PList', 'PMap', 'PSet', 'PVector']
+    T = TypeVar('T')
+    KT = TypeVar('KT')
+    VT = TypeVar('VT')
 
+    # PSet.add and PSet.discard have different type signatures than that of Set.
+    class CheckedPSet(Generic[T], Hashable):
+        pass
 
-T = TypeVar('T')
-KT = TypeVar('KT')
-VT = TypeVar('VT')
+    class CheckedPVector(Sequence[T], Hashable):
+        pass
 
+    class PBag(Container[T], Iterable[T], Sized, Hashable):
+        pass
 
-# PSet.add and PSet.discard have different type signatures than that of Set.
-class CheckedPSet(Generic[T], Hashable):
-    pass
+    class PDeque(Sequence[T], Hashable):
+        pass
 
+    class PList(Sequence[T], Hashable):
+        pass
 
-class CheckedPVector(Sequence[T], Hashable):
-    pass
+    class PMap(Mapping[KT, VT], Hashable):
+        pass
 
+    # PSet.add and PSet.discard have different type signatures than that of Set.
+    class PSet(Generic[T], Hashable):
+        pass
 
-class PBag(Container[T], Iterable[T], Sized, Hashable):
-    pass
-
-
-class PDeque(Sequence[T], Hashable):
-    pass
-
-
-class PList(Sequence[T], Hashable):
-    pass
-
-
-class PMap(Mapping[KT, VT], Hashable):
-    pass
-
-
-# PSet.add and PSet.discard have different type signatures than that of Set.
-class PSet(Generic[T], Hashable):
-    pass
-
-
-class PVector(Sequence[T], Hashable):
+    class PVector(Sequence[T], Hashable):
+        pass
+except ImportError:
     pass
