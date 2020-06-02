bf32c44c978cebe419f03171dfd05a6f03829f08
Tobias Gustafsson <tobias.l.gustafsson@gmail.com>
2020-06-02 07:15:41 +0200

    Update test dependencies memory-profiler and psutil

--- tests/memory_profiling.py
+++ tests/memory_profiling.py
@@ -17,16 +17,19 @@ except ImportError:
 
 PROFILING_DURATION = 2.0
 
+
 def run_function(fn):
     stop = time.time() + PROFILING_DURATION
     while time.time() < stop:
         fn(pvector)
+
 
 def detect_memory_leak(samples):
     # Do not allow a memory usage difference larger than 5% between the beginning and the end.
     # Skip the first samples to get rid of the build up period and the last sample since it seems
     # a little less precise
     return abs(1 - (sum(samples[5:8]) / sum(samples[-4:-1]))) > 0.05
+
 
 def profile_tests():
     test_functions = [fn for fn in inspect.getmembers(vector_test, inspect.isfunction)
@@ -39,6 +42,7 @@ def profile_tests():
             print('Executing %s' % name)
             result = memory_profiler.memory_usage((run_function, (fn,), {}), interval=.1)
             assert not detect_memory_leak(result), (name, result)
+
 
 if __name__ == "__main__":
     profile_tests()
